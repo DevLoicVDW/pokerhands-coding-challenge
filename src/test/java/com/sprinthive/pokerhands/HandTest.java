bb71fbd82ca5e1d42efecd2052894209d1ae033f
LOIC <22538992@sun.ac.za>
2024-03-22 11:27:41 +0200

    Removed unused import.

--- src/test/java/com/sprinthive/pokerhands/HandTest.java
+++ src/test/java/com/sprinthive/pokerhands/HandTest.java
@@ -3,7 +3,6 @@ package com.sprinthive.pokerhands;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
